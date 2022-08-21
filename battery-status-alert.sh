a4939250ae538d95be30c617d0abe681b917aa48
Elbin <elbin.p@gmail.com>
2022-08-21 07:07:20 +0530

    use upower to get battery status

--- battery-status-alert.sh
+++ battery-status-alert.sh
@@ -2,8 +2,12 @@
    while true
     do
        export DISPLAY=:0.0
-       battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
+       battery_path=`upower -e | grep 'BAT'`
+       echo "battery_path $battery_path"
+       battery_level=`upower -i $battery_path|grep percentage|grep -P -o '[0-9]+(?=%)'`
+       echo "battery_level $battery_level"
        adapter_status=`acpi -a`
+       echo "adapter_status $adapter_status"
        if [[ $adapter_status == *"on-line"* ]]; then
            if [ $battery_level -ge 90 ]; then
               notify-send "Battery Full" "Level: ${battery_level}% "
